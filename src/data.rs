5df4eb57286b577d3ecfe2f29d7fd6c8516d77d9
Celeo <mattboulanger@fastmail.com>
2021-08-07 14:18:07 -0700

    progress

--- src/data.rs
+++ src/data.rs
@@ -1,1 +1,27 @@
-// TODO
+use anyhow::Result;
+use sqlx::{Connection, Executor, SqliteConnection};
+use std::{fs::File, path::Path};
+
+const DB_FILE_NAME: &str = "data.db";
+const SQL_CREATE_TABLE: &str = "
+    CREATE TABLE IF NOT EXISTS pastes (
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        content TEXT,
+        lang TEXT,
+        update_code TEXT,
+        add_date DATETIME,
+        purge_date DATETIME
+    );
+";
+
+pub async fn get_db() -> std::result::Result<SqliteConnection, sqlx::Error> {
+    SqliteConnection::connect(DB_FILE_NAME).await
+}
+
+pub async fn setup_db() -> Result<()> {
+    if !Path::new(DB_FILE_NAME).exists() {
+        File::create(DB_FILE_NAME)?;
+    }
+    get_db().await?.execute(SQL_CREATE_TABLE).await?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use axum::prelude::*;
-use std::net::SocketAddr;
+use log::{debug, info};
+use std::{net::SocketAddr, path::Path};
 
 mod data;
+use data::setup_db;
 
 #[tokio::main]
 async fn main() {
-    let app = route("/", get(root));
+    if Path::new(".env").exists() {
+        dotenv::dotenv().expect("Could not load .env file");
+    }
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "pastey=info");
+    }
+    pretty_env_logger::init();
 
+    debug!("Setting up db");
+    setup_db().await.expect("Could not setup DB");
+
+    debug!("Setting up service");
+    let app = route("/", get(root));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    info!("Starting");
     hyper::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
@@ -17,5 +32,3 @@ async fn main() {
 async fn root() -> &'static str {
     "Hello, World!"
 }
-
-// TODO ...
